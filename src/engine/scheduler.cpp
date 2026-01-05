44b60194f46e979b00aaab8845864fd561ce85d2
GaoXiangYa <gx19991105@outlook.com>
2026-01-05 19:40:33 +0800

    add class Scheduler Schedule member function

--- src/engine/scheduler.cpp
+++ src/engine/scheduler.cpp
@@ -1,6 +1,13 @@
 #include "engine/scheduler.h"
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <memory>
+#include <utility>
+#include <vector>
 #include "config.h"
 #include "engine/block_manager.h"
+#include "engine/sequence.h"
 
 namespace engine {
 Scheduler::Scheduler(const Config& config)
@@ -9,5 +16,59 @@ Scheduler::Scheduler(const Config& config)
       eos_(config.eos) {
   block_manager_ = std::make_unique<BlockManager>(config.num_kvcache_blocks,
                                                   config.kvcache_block_size);
+}
+
+auto Scheduler::Schedule() {
+  // prefill
+  std::vector<std::shared_ptr<Sequence>> scheduled_seqs;
+  size_t num_seqs = 0;
+  size_t num_batched_tokens = 0;
+
+  while (!this->waiting_.empty() && num_seqs < this->max_num_seqs_) {
+    auto& seq = this->waiting_.front();
+    if (num_batched_tokens + seq->SequenceLen() >
+            this->max_num_bathced_tokens_ ||
+        !this->block_manager_->CanAllocate(seq.get())) {
+      break;
+    }
+    ++num_seqs;
+    this->block_manager_->Allocate(seq.get());
+    num_batched_tokens += seq->SequenceLen() - seq->GetNumCachedTokens();
+    seq->SetStatus(SequenceStatus::RUNNING);
+    seq = std::move(this->waiting_.front());
+    this->waiting_.pop_front();
+    this->running_.push_back(seq);
+    scheduled_seqs.push_back(seq);
+  }
+  if (!scheduled_seqs.empty()) {
+    return std::make_pair(scheduled_seqs, true);
+  }
+
+  // decode
+  while (!this->running_.empty() && num_seqs < this->max_num_seqs_) {
+    auto seq = std::move(this->running_.front());
+    this->running_.pop_front();
+    while (!this->block_manager_->CanAppend(seq.get())) {
+      if (!this->running_.empty()) {
+        this->Preempt(this->running_.back().get());
+        this->running_.pop_back();
+      } else {
+        this->Preempt(seq.get());
+        seq = nullptr;
+        break;
+      }
+    }
+    if (seq) {
+      ++num_seqs;
+      this->block_manager_->Append(seq.get());
+      scheduled_seqs.push_back(seq);
+    }
+  }
+
+  assert(!scheduled_seqs.empty());
+  std::for_each(scheduled_seqs.rbegin(), scheduled_seqs.rend(), [&](const auto &val) {
+    this->running_.push_front(val);
+  });
+  return std::make_pair(scheduled_seqs, false);
 }
 }  // namespace engine
--- src/include/engine/scheduler.h
+++ src/include/engine/scheduler.h
@@ -19,11 +19,11 @@ public:
     return this->waiting_.empty() && this->running_.empty();
   }
 
-  void Add(std::unique_ptr<Sequence> seq) { this->waiting_.push_back(seq); }
+  void Add(const std::shared_ptr<Sequence>& seq) { this->waiting_.push_back(seq); }
 
-  std::pair<std::vector<std::unique_ptr<Sequence>>, bool> Schedule();
+  auto Schedule();
 
-  void Preempt(std::unique_ptr<Sequence> seq);
+  void Preempt(const Sequence* seq);
 
   void PostProcess(const std::vector<std::unique_ptr<Sequence>>& seqs,
                    const std::vector<size_t>& token_ids);
@@ -33,8 +33,8 @@ private:
   size_t max_num_bathced_tokens_;
   size_t eos_;
   std::unique_ptr<BlockManager> block_manager_;
-  std::deque<std::unique_ptr<Sequence>> waiting_{};
-  std::deque<std::unique_ptr<Sequence>> running_{};
+  std::deque<std::shared_ptr<Sequence>> waiting_{};
+  std::deque<std::shared_ptr<Sequence>> running_{};
 };
 
 }  // namespace engine
