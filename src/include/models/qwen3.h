04786aa71acf099b605206da06130543abb82562
GaoXiangYa <gx19991105@outlook.com>
2026-01-07 23:22:24 +0800

    add qwen3.h

---
+++ src/include/models/qwen3.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include "ggml-alloc.h"
+#include "ggml-backend.h"
+#include "ggml.h"
+
+namespace models {
+
+struct Qwen3Hparams {
+  static constexpr int32_t n_vocab = 151936;
+  static constexpr int32_t n_layer = 28;
+  static constexpr int32_t n_ctx = 40960;
+  static constexpr int32_t n_embd = 1024;
+  static constexpr int32_t n_embd_head = 128;
+  static constexpr float rms_eps = 1e-6;
+};
+
+struct Qwen3Layer {
+  // normalization
+  struct ggml_tensor* attn_norm = nullptr;
+  struct ggml_tensor* attn_q_norm = nullptr;
+  struct ggml_tensor* attn_k_norm = nullptr;
+
+  // attention
+  struct ggml_tensor* wq = nullptr;
+  struct ggml_tensor* wk = nullptr;
+  struct ggml_tensor* wv = nullptr;
+  struct ggml_tensor* wo = nullptr;
+
+  // normalization
+  struct ggml_tensor* ffn_norm = nullptr;
+
+  // ff
+  struct ggml_tensor* ffn_gate = nullptr;  // w1
+  struct ggml_tensor* ffn_down = nullptr;  // w2
+  struct ggml_tensor* ffn_up = nullptr;    // w3
+};
+
+class Qwen3Model {
+public:
+  explicit Qwen3Model(const std::string& model_path);
+
+private:
+  Qwen3Hparams hparams;
+
+  struct ggml_tensor* tok_embd = nullptr;
+  struct ggml_tensor* output = nullptr;
+  struct ggml_tensor* output_norm = nullptr;
+
+  std::vector<Qwen3Layer> layers;
+
+  ggml_backend_t backend = nullptr;
+  ggml_backend_buffer_t buffer_w;
+  ggml_backend_buffer_t buffer_kv;
+};
+
+}  // namespace models
